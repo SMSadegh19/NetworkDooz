9493da93a9fe43619c708d01581e3154b924c888
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-05-27 07:46:00 +0430

    Server connecting to clients implemented.

--- src/DoozServer.java
+++ src/DoozServer.java
@@ -1,12 +1,20 @@
+import models.ClientSecretary;
+import models.ServerContents;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.Buffer;
 
 public class DoozServer {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         ServerSocket serverSocket = new ServerSocket(8888);
-        ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+        while (true) {
+            System.out.println("Server is waiting for new client...");
+            Socket socket = serverSocket.accept();
+            ClientSecretary newClientSecretary = new ClientSecretary(socket);
+            ServerContents.addClient(newClientSecretary);
+            newClientSecretary.startWorking();
+        }
     }
 }
